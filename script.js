75fcda8589c5add754189d094aa6b1655266f58b
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-25 11:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	const char = `ABCDEFGHIJKLMNPOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`;
+	let result="";
+	for(let  i=0;i<l;i++){
+		char rIdx = Math.floor(Math.random() * char.length);
+		result+=char[rIdx];
+	}
+	return result;
 }
 
 // Do not change the code below.
